132a784db593ea149aaf924588c08f111e7ff391
Gloria Chan <gloriachan@GloriadeMacBook-Air.local>
2025-10-15 08:33:59 +0800

    更新构建产物：同步最新Flutter构建到web目录

--- 全球法布施/web/.wrangler/tmp/bundle-BQBAIi/middleware-loader.entry.ts
+++ 全球法布施/web/.wrangler/tmp/bundle-BQBAIi/middleware-loader.entry.ts
@@ -3,12 +3,12 @@
 // export dynamically through wrangler, or we can potentially let users directly
 // add them as a sort of "plugin" system.
 
-import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "/Users/gloriachan/Documents/全球发送/全球法布施/web/.wrangler/tmp/bundle-r02JVv/middleware-insertion-facade.js";
+import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "/Users/gloriachan/Documents/全球发送/全球法布施/web/.wrangler/tmp/bundle-BQBAIi/middleware-insertion-facade.js";
 import { __facade_invoke__, __facade_register__, Dispatcher } from "/Users/gloriachan/.npm-global/lib/node_modules/wrangler/templates/middleware/common.ts";
-import type { WorkerEntrypointConstructor } from "/Users/gloriachan/Documents/全球发送/全球法布施/web/.wrangler/tmp/bundle-r02JVv/middleware-insertion-facade.js";
+import type { WorkerEntrypointConstructor } from "/Users/gloriachan/Documents/全球发送/全球法布施/web/.wrangler/tmp/bundle-BQBAIi/middleware-insertion-facade.js";
 
 // Preserve all the exports from the worker
-export * from "/Users/gloriachan/Documents/全球发送/全球法布施/web/.wrangler/tmp/bundle-r02JVv/middleware-insertion-facade.js";
+export * from "/Users/gloriachan/Documents/全球发送/全球法布施/web/.wrangler/tmp/bundle-BQBAIi/middleware-insertion-facade.js";
 
 class __Facade_ScheduledController__ implements ScheduledController {
 	readonly #noRetry: ScheduledController["noRetry"];
