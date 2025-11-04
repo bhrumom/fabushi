155ea559434c1750a238acd43121d788a152f33c
Gloria Chan <gloriachan@fabushi.com>
2025-11-04 21:40:16 +0800

    Fix: 修复Web构建和部署问题，移除大文件，添加assets路由支持

--- 全球法布施/macos/Flutter/GeneratedPluginRegistrant.swift
+++ 全球法布施/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -21,7 +21,6 @@ import path_provider_foundation
 import screen_retriever_macos
 import shared_preferences_foundation
 import sqflite_darwin
-import sqlite3_flutter_libs
 import url_launcher_macos
 import video_player_avfoundation
 import webview_flutter_wkwebview
@@ -44,7 +43,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
-  Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
   WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
