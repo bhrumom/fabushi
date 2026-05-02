6141c458c7146ef77a67ebcde0450529c5ab6c9f
bhrum <bhrumom@gmail.com>
2026-05-02 09:37:56 +0800

    style: format app config

--- fabushi/lib/core/config/app_config.dart
+++ fabushi/lib/core/config/app_config.dart
@@ -33,7 +33,8 @@ class AppConfig {
   // API配置
   static const String primaryBackendUrl = 'https://flutter.ombhrum.com';
   static const String cloudflareWorkerProdUrl = 'https://flutter.ombhrum.com';
-  static const String cloudflareWorkerDevUrl = 'https://fabushi-flutter-web-dev.bhrumom.workers.dev';
+  static const String cloudflareWorkerDevUrl =
+      'https://fabushi-flutter-web-dev.bhrumom.workers.dev';
   static const String localDevUrl = 'http://localhost:8787';
 
   static String get currentBackendUrl {
