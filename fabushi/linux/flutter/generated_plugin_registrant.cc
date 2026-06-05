5fc26a6931e5eb3ad08e60c851fef8e5108e63b0
bhrum <bhrumom@gmail.com>
2026-06-05 09:40:57 +0800

    Add embedded OpenClaw desktop installers CI (#727)
    * Add embedded OpenClaw desktop installers CI
    
    * Fix desktop installer sparse checkout
    
    ---------
    
    Co-authored-by: Gloria Chan <gloriachan@fabushi.com>

---
+++ fabushi/linux/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,43 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_volume_controller/flutter_volume_controller_plugin.h>
+#include <gtk/gtk_plugin.h>
+#include <record_linux/record_linux_plugin.h>
+#include <rive_common/rive_plugin.h>
+#include <screen_retriever_linux/screen_retriever_linux_plugin.h>
+#include <taudio/taudio_plugin.h>
+#include <url_launcher_linux/url_launcher_plugin.h>
+#include <window_manager/window_manager_plugin.h>
+
+void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) flutter_volume_controller_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterVolumeControllerPlugin");
+  flutter_volume_controller_plugin_register_with_registrar(flutter_volume_controller_registrar);
+  g_autoptr(FlPluginRegistrar) gtk_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "GtkPlugin");
+  gtk_plugin_register_with_registrar(gtk_registrar);
+  g_autoptr(FlPluginRegistrar) record_linux_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "RecordLinuxPlugin");
+  record_linux_plugin_register_with_registrar(record_linux_registrar);
+  g_autoptr(FlPluginRegistrar) rive_common_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "RiveCommonPlugin");
+  rive_plugin_register_with_registrar(rive_common_registrar);
+  g_autoptr(FlPluginRegistrar) screen_retriever_linux_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "ScreenRetrieverLinuxPlugin");
+  screen_retriever_linux_plugin_register_with_registrar(screen_retriever_linux_registrar);
+  g_autoptr(FlPluginRegistrar) flutter_sound_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterSoundPlugin");
+  taudio_plugin_register_with_registrar(flutter_sound_registrar);
+  g_autoptr(FlPluginRegistrar) url_launcher_linux_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "UrlLauncherPlugin");
+  url_launcher_plugin_register_with_registrar(url_launcher_linux_registrar);
+  g_autoptr(FlPluginRegistrar) window_manager_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "WindowManagerPlugin");
+  window_manager_plugin_register_with_registrar(window_manager_registrar);
+}
