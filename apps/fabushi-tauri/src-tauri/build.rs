130d61e95e33f6833c52e9b237b1c16ec8daf8aa
bhrum <bhrumom@gmail.com>
2026-08-14 22:11:38 +0800

    test(mac): gate WebDriver shell with build-time cfg

--- apps/fabushi-tauri/src-tauri/build.rs
+++ apps/fabushi-tauri/src-tauri/build.rs
@@ -1,4 +1,8 @@
 fn main() {
+    println!("cargo:rustc-check-cfg=cfg(fabushi_tauri_wdio_e2e)");
+    if std::env::var_os("FABUSHI_TAURI_WDIO_E2E").is_some() {
+        println!("cargo:rustc-cfg=fabushi_tauri_wdio_e2e");
+    }
     if std::env::var_os("CARGO_FEATURE_DESKTOP").is_some() {
         tauri_build::build();
     }
