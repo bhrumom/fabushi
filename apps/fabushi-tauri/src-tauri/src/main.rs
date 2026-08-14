c209d76184f83c2221f5dbe0d2f5c51003995544
bhrum <bhrumom@gmail.com>
2026-08-14 22:11:56 +0800

    test(mac): select E2E shell without product feature

--- apps/fabushi-tauri/src-tauri/src/main.rs
+++ apps/fabushi-tauri/src-tauri/src/main.rs
@@ -1,12 +1,12 @@
-#[cfg(feature = "tauri-wdio-e2e")]
+#[cfg(fabushi_tauri_wdio_e2e)]
 mod e2e;
 
-#[cfg(feature = "tauri-wdio-e2e")]
+#[cfg(fabushi_tauri_wdio_e2e)]
 fn main() {
     e2e::run();
 }
 
-#[cfg(not(feature = "tauri-wdio-e2e"))]
+#[cfg(not(fabushi_tauri_wdio_e2e))]
 fn main() {
     fabushi_tauri_lib::run();
 }
